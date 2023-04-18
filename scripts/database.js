fe617608730fc30b9a0ed53a91c9bca0eaffc7de
William Briscoe <will.m.briscoe@gmail.com>
2023-04-18 13:29:42 -0500

    lastcommit

---
+++ scripts/database.js
@@ -0,0 +1,34 @@
+const database = {
+    heroes: [
+        {
+            id: 1,
+            name: "Dynoguy",
+            power: "Fire blasts"
+        },
+        {
+            id: 2,
+            name: "Hyperspeed",
+            power: "Super speed"
+        }
+    ],
+    villains: [
+        {
+            id: 1,
+            name: "Syndrone",
+            power: "Infintry"
+        },
+        {
+            id: 2,
+            name: "Freeza",
+            power: "Alien chi"
+        }
+    ]
+}
+
+export const getHeroes = () => {
+    return database.heroes.map(hero => ({...hero}))
+}
+
+export const getVillains = () => {
+    return database.villains.map(villain => ({...villain}))
+}
